2064985ddc5caf915ee9a52c67e2aead97ac4039
NoelleBelle23 <DogWhisperer125@comcast.net>
2017-12-02 12:47:24 -0800

    Basic Cam stuff hopefully

--- app/src/main/java/threw/dat/away/Main.java
+++ app/src/main/java/threw/dat/away/Main.java
@@ -1,13 +1,37 @@
 package threw.dat.away;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
+    boolean hasCam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hasCam = checkCameraHardware(this);
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
